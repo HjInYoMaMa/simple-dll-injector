a58ceb6b3a19880267001fb5783fea91e175f520
hj <88996806+HjInYoMaMa@users.noreply.github.com>
2022-06-10 19:28:12 +0300

    Add files via upload

---
+++ dll-injector/dll-injector.cpp
@@ -0,0 +1,69 @@
+#include <Windows.h>
+#include <iostream>
+#include <TlHelp32.h>
+using namespace std;
+DWORD GetProcId(const char* procName)
+{
+    DWORD procId = 0;
+    HANDLE hSnap = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
+
+    if (hSnap != INVALID_HANDLE_VALUE)
+    {
+        PROCESSENTRY32 procEntry;
+        procEntry.dwSize = sizeof(procEntry);
+
+        if (Process32First(hSnap, &procEntry))
+        {
+            do
+            {
+                if (!_stricmp(procEntry.szExeFile, procName))
+                {
+                    procId = procEntry.th32ProcessID;
+                    break;
+                }
+            } while (Process32Next(hSnap, &procEntry));
+        }
+    }
+    CloseHandle(hSnap);
+    return procId;
+}
+
+int main()
+{
+    SetConsoleTitleA("Bliss on bliss on bliss on");
+    std::cout << "Waiting for Process_Name.exe!";
+    const char* dllPath = "C:\\Users\\hj.\\Desktop\\Name.dll"; //dllpath, this is example.
+    const char* procName = "Process_Name.exe"; //process name
+    DWORD procId = 0;
+
+    while (!procId)
+    {
+        procId = GetProcId(procName);
+        Sleep(30);
+    }
+
+    HANDLE hProc = OpenProcess(PROCESS_ALL_ACCESS, 0, procId);
+
+    if (hProc && hProc != INVALID_HANDLE_VALUE)
+    {
+        void* loc = VirtualAllocEx(hProc, 0, MAX_PATH, MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE);
+
+        if (loc)
+        {
+            WriteProcessMemory(hProc, loc, dllPath, strlen(dllPath) + 1, 0);
+        }
+
+        HANDLE hThread = CreateRemoteThread(hProc, 0, 0, (LPTHREAD_START_ROUTINE)LoadLibraryA, loc, 0, 0);
+
+        if (hThread)
+        {
+            CloseHandle(hThread);
+        }
+    }
+
+    if (hProc)
+    {
+        CloseHandle(hProc);
+    }
+    return 0;
+}
